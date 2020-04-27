32be7fc07f6561ac1876fb1bc75870b583f07237
dedovich-s <dedovich.serge@gmail.com>
2020-04-27 17:11:14 +0300

    ui controller added, react ui included in build

---
+++ src/main/java/com/dedovich/localization/conroller/UiController.java
@@ -0,0 +1,10 @@
+package com.dedovich.localization.conroller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UiController {
+    public String index() {
+        return "index";
+    }
+}
